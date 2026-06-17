c83948515e4be1b131b5e0033f32a2b720214c7f
jasper-workspace <huanjun_zhang0806@163.com>
2026-06-17 17:05:50 +0800

    fix 处理构建失败的问题

---
+++ scripts/set-env.js
@@ -0,0 +1,4 @@
+// 设置 electron-builder 镜像环境变量
+process.env.ELECTRON_MIRROR = 'https://npmmirror.com/mirrors/electron/';
+process.env.ELECTRON_BUILDER_BINARIES_MIRROR = 'https://npmmirror.com/mirrors/electron-builder-binaries/';
+process.env.ELECTRON_BUILDER_FULL_NPM_MIRROR_URL = 'https://npmmirror.com/mirrors/electron-builder-binaries/';
